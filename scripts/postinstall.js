4e3db2cd9fec1f9ea4bd714e84f437d081c1f49f
Theryston <fuktodo2@gmail.com>
2022-09-06 15:42:56 -0300

    fix: add post install

---
+++ scripts/postinstall.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process')
+
+try {
+  execSync('prisma generate')
+} catch (error) {
+  console.log('Prisma generate failed', error)
+}
+
+try {
+  execSync('tiktoker config')
+} catch (e) {
+  console.log('Has an error to run "tiktoker config", please run it manually')
+}
